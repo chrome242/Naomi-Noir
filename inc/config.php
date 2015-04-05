d9b55b5aa5af0a42b18eaf332084bf817c42467a
William LaMorie <wjl10@cornell.edu>
2015-04-04 21:25:08 -0400

    removed junk, added todo list.
    -Added TODO list to config.php
    -Removed Junk folders
    -Removed folders for items that are being pulled from CDN

--- inc/config.php
+++ inc/config.php
@@ -1,5 +1,5 @@
 <?php
-    // the root url of the site relative to the server root. Will be / on the local server
+  // the root url of the site relative to the server root. Will be / on the local server
   define("BASE_URL", "/"); // for dev launch
   
   // the root path .. base_url should usally equal the concat to the end of the DOCROOT
@@ -11,6 +11,57 @@
   define("RESUME", "Resume/");
   define("PORTFOLIO", "Portfolio/");
   define("PRODUCTIONS", "Productions/");
+  define("CAST", "Cast/");
+  define("CREW", "Crew/");
+  define("MAKING", "Making/");
   define("DESIGN", "Design/");
   define("CLASSES", "Classes/");
   
+  //TODO LIST:
+  
+  // move Nav bar to include, remove home due to name click == home
+  
+  //TODO: join all CSS files into a css/style file
+    // css
+    // about
+    // cast
+    // classes
+    // crew
+    // design
+    // making
+    // portfolio
+    // productions
+    // resume
+    
+  //TODO: move over from small case to Capitalize directory strucutre, updating as we go
+    // about
+    // cast
+    // classes
+    // crew
+    // design
+    // making
+    // portfolio
+    // productions
+    // resume
+    
+  //TODO: remove auto drop of the first section in the design section
+  
+  //TODO: Update main page: FLOT, FLAG, about WJL
+  
+  //TODO: update footer
+  
+  //TODO: redirect rules for changed locations (301's)
+  
+  //TODO: fix the slideshow on the classes
+  
+  //TODO: Update about
+  
+  //TODO: Update CV
+  
+  //TODO: Update About
+  
+  //TODO: Update Making Of
+  
+  //TODO: Consider making both pdf templates one in the same
+  
+  //TODO: Remove templates when the time is right
